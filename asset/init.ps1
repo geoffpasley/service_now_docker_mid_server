a539aed550148ad3ae9a975821ef74ccdff27029
geoffpasley <geoffpasley@gmail.com>
2019-09-20 11:12:35 -0700

    init file update
    Change logic to init file.

--- asset/init.ps1
+++ asset/init.ps1
@@ -6,19 +6,69 @@ param(
 	[string]$mid_server_name = $env:sn_mid_server_name
 )
 
-if(!(Test-Path $config_file))
+function check-variables
 {
-	COPY C:\asset\config.xml C:\agent\
-	[xml]$myXML = Get-Content $config_file
-	$myXML.parameters.ChildNodes.Item(2).value = $url
-	$myXML.parameters.ChildNodes.Item(4).value = $username
-	$myXML.parameters.ChildNodes.Item(5).value = $password
-	$myXML.parameters.ChildNodes.Item(7).value = $mid_server_name
+    $e = $false;
 
-	$myXML.Save($config_file)
+    if($url -eq "" -or $username -eq "" -or $password -eq "" -or $mid_server_name -eq "")
+    {
+        $e = $true;
+    }
+
+    return $e;
 }
 
-cd C:\agent\
-.\start.bat
+function update-config-file
+{
+    $e = check-variables;
 
-ping localhost -t
+    if($e)
+    {
+        Write-Host "You must provide all environment variables to build config.xml file." -ForegroundColor Red;
+    }
+    else
+    {
+        try
+        {
+            COPY C:\asset\config.xml C:\agent\;
+	        [xml]$myXML = Get-Content $config_file
+	        $myXML.parameters.ChildNodes.Item(2).value = $url;
+	        $myXML.parameters.ChildNodes.Item(4).value = $username;
+	        $myXML.parameters.ChildNodes.Item(5).value = $password;
+	        $myXML.parameters.ChildNodes.Item(7).value = $mid_server_name;
+
+	        $myXML.Save($config_file);
+            Write-Host "Configuration file built." -ForegroundColor Green;
+        }
+        catch
+        {
+            $e = $true;
+        }
+    }
+
+    return $e;
+}
+
+$error = $false;
+
+if(!(Test-Path $config_file))
+{
+	$error = update-config-file;
+}
+
+if(!$error)
+{
+    Write-Host "Conecting to: $url";
+    Write-Host "Mid Server name: $mid_server_name";
+    Write-Host "";
+
+    cd C:\agent\
+    .\start.bat
+
+    Write-Host "Container Started..." -ForegroundColor Green;
+
+    while (0 -eq 0)
+    {
+        Start-Sleep -s 10;
+    }
+}
